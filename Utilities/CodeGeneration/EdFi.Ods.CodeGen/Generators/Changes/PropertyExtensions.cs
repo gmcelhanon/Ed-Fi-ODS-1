5bce2aa5dd7916b79a9d395ad0c66b408d0b9f31
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-08-16 15:12:10 -0500

    Adding Ed-Fi license headers.

--- Utilities/CodeGeneration/EdFi.Ods.CodeGen/Generators/Changes/PropertyExtensions.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen/Generators/Changes/PropertyExtensions.cs
@@ -1,9 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 using System.Collections.Generic;
 using System.Linq;
 using EdFi.Ods.CodeGen.Extensions;
 using EdFi.Ods.CodeGen.Generators.Changes.Models;
-using EdFi.Ods.CodeGen.Models;
-using EdFi.Ods.Common.Extensions;
 using EdFi.Ods.Common.Models.Domain;
 
 namespace EdFi.Ods.CodeGen.Generators.Changes 
