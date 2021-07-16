3bedd06dcc5bad438803ee965ae50e500544f825
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2021-07-16 16:34:14 -0500

    Porting ChangeQueries related artifacts to v5.2 branch (non-compiling).

--- Application/EdFi.Ods.Features/ChangeQueries/Resources/DeletedResourceItem.cs
+++ Application/EdFi.Ods.Features/ChangeQueries/Resources/DeletedResourceItem.cs
@@ -1,19 +1,25 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
+using EdFi.Ods.Common;
+using Newtonsoft.Json;
 
-namespace EdFi.Ods.Common.Models
+namespace EdFi.Ods.Features.ChangeQueries.Resources
 {
-    public class DeletedResource
+    public class DeletedResourceItem
     {
         [DataMember(Name="id")]
+        [JsonConverter(typeof(GuidConverter))]
         public Guid Id { get; set; }
 
         [DataMember(Name="changeVersion")]
         public long ChangeVersion { get; set; }
-    }
-}
+
+        [DataMember(Name="keyValues")]
+        public IDictionary<string, object> KeyValues { get; set; }
+    }}
