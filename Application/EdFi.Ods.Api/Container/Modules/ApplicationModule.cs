d9439c944358dad2394d03c7c1997b0bfafe8fa5
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2021-07-17 00:20:35 -0500

    Completed removal of "dead code" for pipeline artifacts related to ChangeQueries' deleted ids.

--- Application/EdFi.Ods.Api/Container/Modules/ApplicationModule.cs
+++ Application/EdFi.Ods.Api/Container/Modules/ApplicationModule.cs
@@ -262,10 +262,6 @@ namespace EdFi.Ods.Api.Container.Modules
                     .AsSelf()
                     .SingleInstance();
 
-                builder.RegisterGeneric(typeof(GetDeletedResourceModelByIds<,,>))
-                    .AsSelf()
-                    .SingleInstance();
-
                 builder.RegisterGeneric(typeof(ValidateResourceModel<,,,>))
                     .AsSelf()
                     .SingleInstance();
@@ -296,11 +292,6 @@ namespace EdFi.Ods.Api.Container.Modules
 
                 builder.RegisterType<GetBySpecificationPipelineStepsProvider>()
                     .As<IGetBySpecificationPipelineStepsProvider>()
-                    .As<IPipelineStepsProvider>()
-                    .SingleInstance();
-
-                builder.RegisterType<GetDeletedResourceIdsPipelineStepsProvider>()
-                    .As<IGetDeletedResourceIdsPipelineStepsProvider>()
                     .As<IPipelineStepsProvider>()
                     .SingleInstance();
 
