96942affc2e372437ff37d911acab4b1ad09367f
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-04-14 21:38:07 -0500

    Various changes, too many to enumerate.

--- Application/EdFi.Ods.Common/Configuration/ApiSettings.cs
+++ Application/EdFi.Ods.Common/Configuration/ApiSettings.cs
@@ -14,27 +14,11 @@ namespace EdFi.Ods.Common.Configuration
     public class ApiSettings
     {
         private readonly Lazy<DatabaseEngine> _databaseEngine;
-        private readonly Lazy<ApiMode> _apiMode;
 
         public ApiSettings()
         {
             _databaseEngine = new Lazy<DatabaseEngine>(() => DatabaseEngine.TryParseEngine(Engine));
-
-            _apiMode = new Lazy<ApiMode>(
-                () =>
-                {
-                    if (ApiMode.TryParse(x => x.Value.EqualsIgnoreCase(Mode), out ApiMode apiMode))
-                    {
-                        return apiMode;
-                    }
-
-                    throw new NotSupportedException(
-                        $"Not supported Mode \"{Mode}\". Supported modes: {ApiConfigurationConstants.Sandbox}, {ApiConfigurationConstants.YearSpecific}, and {ApiConfigurationConstants.SharedInstance}.");
-                }
-            );
         }
-
-        public string Mode { get; set; }
 
         public string Engine { get; set; }
 
@@ -42,11 +26,11 @@ namespace EdFi.Ods.Common.Configuration
 
         public int[] Years { get; set; }
 
-        public List<Feature> Features { get; set; } = new List<Feature>();
+        public List<Feature> Features { get; set; } = new();
 
-        public List<ScheduledJobSetting> ScheduledJobs { get; set; } = new List<ScheduledJobSetting>();
+        public List<ScheduledJobSetting> ScheduledJobs { get; set; } = new();
 
-        public List<string> ExcludedExtensions { get; set; } = new List<string>();
+        public List<string> ExcludedExtensions { get; set; } = new();
 
         public bool? UseReverseProxyHeaders { get; set; }
 
@@ -54,18 +38,19 @@ namespace EdFi.Ods.Common.Configuration
 
         public int? OverrideForForwardingHostPort { get; set; }
 
-        public ReverseProxySettings GetReverseProxySettings()
-        {
-            return new ReverseProxySettings(this.UseReverseProxyHeaders, this.OverrideForForwardingHostServer, this.OverrideForForwardingHostPort);
-        }
-
         public string PathBase { get; set; }
 
-        public CacheSettings Caching { get; set; } = new CacheSettings();
+        public CacheSettings Caching { get; set; } = new();
+
+        public ReverseProxySettings GetReverseProxySettings()
+        {
+            return new ReverseProxySettings(
+                UseReverseProxyHeaders,
+                OverrideForForwardingHostServer,
+                OverrideForForwardingHostPort);
+        }
 
         public DatabaseEngine GetDatabaseEngine() => _databaseEngine.Value;
-
-        public ApiMode GetApiMode() => _apiMode.Value;
 
         public bool IsFeatureEnabled(string featureName)
             => Features.SingleOrDefault(x => x.Name.EqualsIgnoreCase(featureName) && x.IsEnabled) != null;
