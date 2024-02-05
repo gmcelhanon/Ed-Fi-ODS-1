c40778ba1cd634eb49848366c87d7674c853cb05
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2024-02-05 13:17:33 -0600

    Removed interceptor-based cache key not used in v7.1.

--- Application/EdFi.Ods.Common/Caching/InterceptorCacheKeys.cs
+++ Application/EdFi.Ods.Common/Caching/InterceptorCacheKeys.cs
@@ -7,5 +7,4 @@ public static class InterceptorCacheKeys
     public const string OdsInstances = "cache-ods-instances";
     public const string ProfileMetadata = "cache-profile-metadata";
     public const string ApiClientDetails = "cache-api-client-details";
-    public const string ModelStateKey = "cache-model-state-key";
 }
